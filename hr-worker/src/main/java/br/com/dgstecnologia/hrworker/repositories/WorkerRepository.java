9d5c34d6439cafba14980651c4bd48634ef57421
douglas <douglaslopesfarias@outlook.com>
2021-10-05 18:47:08 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/br/com/dgstecnologia/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,12 @@
+package br.com.dgstecnologia.hrworker.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import br.com.dgstecnologia.hrworker.entities.Worker;
+
+public interface  WorkerRepository extends JpaRepository<Worker, Long> {
+	
+	
+	
+	
+
+}
